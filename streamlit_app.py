bb921d2cbbb9c5995cbb1b4afa835d73b55c6c21
belgrano9 <36034688+belgrano9@users.noreply.github.com>
2022-11-24 16:17:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,12 +12,21 @@ In the meantime, below is an example of what you can do with just a few lines of
 import streamlit as st
 import pandas as pd
 import numpy as np
+import functions
 
 st.title('Project visualization')
+
+st.sidebar.write("Sidebar test")
+st.sidebar.button("Click me")
+
 
 uploaded_file=st.file_uploader("Upload your file")
 if uploaded_file:
    st.write("Filename: ", uploaded_file.name)
+   
+filename=uploaded_file.name
+
+st.write(functions.reading_file(filename))
 
 
 
